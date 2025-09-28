136bc2cc7094242cde40b21c798fc5d56d5493fd
annalevander <annalevander@icloud.com>
2025-09-28 23:15:20 +0200

    Add project files.

---
+++ PyramidBuilder/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace PyramidBuilder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Välkommen");
+            PyramidBuilder();
+        }
+        static void PyramidBuilder()
+        {
+            Console.WriteLine("Vi ska bygga en pyramid, skriv in hur många rader hög den ska vara");
+            int height = int.Parse(Console.ReadLine());
+            for (int row = 1; row <= height; row++)
+            {
+                for (int space = 0; space < height - row; space++)
+                {
+                    Console.Write(" ");
+                }
+                for (int star = 0; star < 2  * row - 1; star++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
