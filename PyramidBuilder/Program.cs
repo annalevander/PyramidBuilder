2f27f4e8c72182e3966a51ebb17dcc8b72edc968
annalevander <annalevander@icloud.com>
2025-10-02 15:10:13 +0200

    added comments

--- PyramidBuilder/Program.cs
+++ PyramidBuilder/Program.cs
@@ -4,23 +4,30 @@
     {
         static void Main(string[] args)
         {
+            //välkomstmeddelande och anropa metoden PyramidBuilder
             Console.WriteLine("Välkommen");
             PyramidBuilder();
         }
         static void PyramidBuilder()
         {
+            //gör en int för att spara hur hög användaren vill att pyramiden ska vara
             Console.WriteLine("Vi ska bygga en pyramid, skriv in hur många rader hög den ska vara");
             int height = int.Parse(Console.ReadLine());
+
+            // yttre loopen gör att den börjar på 1 och lägger på en så länge höjden är större eller samma som row. den kör alltså en gång för varje rad i pyramiden
             for (int row = 1; row <= height; row++)
             {
+                // skriver ut mellanslag så att pyramiden blir centrerad och den lägger till mellanslag ju högre upp i pyramiden man kommer
                 for (int space = 0; space < height - row; space++)
                 {
                     Console.Write(" ");
                 }
+                // skriver ut stjärnorna som bygger pyramiden. då använder man funktionen 2*n-1, där n är min row
                 for (int star = 0; star < 2  * row - 1; star++)
                 {
                     Console.Write("*");
                 }
+                // börjar på en ny rad efter föregående rad är klar
                 Console.WriteLine();
             }
         }
